c23fc41c69f59d4cbce2f3005faa03367a3baacc
quakelele <atwarasulov@gmail.com>
2025-06-24 20:10:00 +0400

    fixed some styles and kostails with select and etc

---
+++ src/features/QuranReader/constants/surahs.ts
@@ -0,0 +1,537 @@
+export const surahOptions = [
+  {
+    value: 1,
+    label: { arabic: 'الفاتحة', russian: 'Открывающая', azerbaijani: 'Fatiha' },
+  },
+  {
+    value: 2,
+    label: { arabic: 'البقرة', russian: 'Корова', azerbaijani: 'Bəqərə' },
+  },
+  {
+    value: 3,
+    label: {
+      arabic: 'آل عمران',
+      russian: 'Семейство Имрана',
+      azerbaijani: 'Əl-İmran',
+    },
+  },
+  {
+    value: 4,
+    label: { arabic: 'النساء', russian: 'Женщины', azerbaijani: 'Nisa' },
+  },
+  {
+    value: 5,
+    label: { arabic: 'المائدة', russian: 'Трапеза', azerbaijani: 'Maidə' },
+  },
+  {
+    value: 6,
+    label: { arabic: 'الأنعام', russian: 'Скот', azerbaijani: 'Ənam' },
+  },
+  {
+    value: 7,
+    label: {
+      arabic: 'الأعراف',
+      russian: 'Перегороженные',
+      azerbaijani: "Ə'raf",
+    },
+  },
+  {
+    value: 8,
+    label: { arabic: 'الأنفال', russian: 'Добыча', azerbaijani: 'Ənfal' },
+  },
+  {
+    value: 9,
+    label: { arabic: 'التوبة', russian: 'Покаяние', azerbaijani: 'Tövbə' },
+  },
+  {
+    value: 10,
+    label: { arabic: 'يونس', russian: 'Юнус', azerbaijani: 'Yunus' },
+  },
+  { value: 11, label: { arabic: 'هود', russian: 'Худ', azerbaijani: 'Hud' } },
+  {
+    value: 12,
+    label: { arabic: 'يوسف', russian: 'Юсуф', azerbaijani: 'Yusif' },
+  },
+  {
+    value: 13,
+    label: { arabic: 'الرعد', russian: 'Гром', azerbaijani: 'Rəd' },
+  },
+  {
+    value: 14,
+    label: { arabic: 'ابراهيم', russian: 'Ибрахим', azerbaijani: 'İbrahim' },
+  },
+  {
+    value: 15,
+    label: { arabic: 'الحجر', russian: 'Камень', azerbaijani: 'Hicr' },
+  },
+  {
+    value: 16,
+    label: { arabic: 'النحل', russian: 'Пчёлы', azerbaijani: 'Nəhl' },
+  },
+  {
+    value: 17,
+    label: {
+      arabic: 'الإسراء',
+      russian: 'Ночное путешествие',
+      azerbaijani: 'İsra',
+    },
+  },
+  {
+    value: 18,
+    label: { arabic: 'الكهف', russian: 'Пещера', azerbaijani: 'Kəhf' },
+  },
+  {
+    value: 19,
+    label: { arabic: 'مريم', russian: 'Мариям', azerbaijani: 'Məryəm' },
+  },
+  { value: 20, label: { arabic: 'طه', russian: 'Таха', azerbaijani: 'Taha' } },
+  {
+    value: 21,
+    label: { arabic: 'الأنبياء', russian: 'Пророки', azerbaijani: 'Ənbiya' },
+  },
+  {
+    value: 22,
+    label: { arabic: 'الحج', russian: 'Паломничество', azerbaijani: 'Həcc' },
+  },
+  {
+    value: 23,
+    label: { arabic: 'المؤمنون', russian: 'Верующие', azerbaijani: 'Mü’minun' },
+  },
+  {
+    value: 24,
+    label: { arabic: 'النور', russian: 'Свет', azerbaijani: 'Nur' },
+  },
+  {
+    value: 25,
+    label: { arabic: 'الفرقان', russian: 'Различение', azerbaijani: 'Furqan' },
+  },
+  {
+    value: 26,
+    label: { arabic: 'الشعراء', russian: 'Поэты', azerbaijani: 'Şuəra' },
+  },
+  {
+    value: 27,
+    label: { arabic: 'النمل', russian: 'Муравьи', azerbaijani: 'Nəml' },
+  },
+  {
+    value: 28,
+    label: { arabic: 'القصص', russian: 'Истории', azerbaijani: 'Qəsəs' },
+  },
+  {
+    value: 29,
+    label: { arabic: 'العنكبوت', russian: 'Паук', azerbaijani: 'Ənkəbut' },
+  },
+  {
+    value: 30,
+    label: { arabic: 'الروم', russian: 'Римляне', azerbaijani: 'Rum' },
+  },
+  {
+    value: 31,
+    label: { arabic: 'لقمان', russian: 'Лукман', azerbaijani: 'Luqman' },
+  },
+  {
+    value: 32,
+    label: { arabic: 'السجدة', russian: 'Поклон', azerbaijani: 'Səcdə' },
+  },
+  {
+    value: 33,
+    label: { arabic: 'الأحزاب', russian: 'Союзники', azerbaijani: 'Əhzab' },
+  },
+  { value: 34, label: { arabic: 'سبإ', russian: 'Саба', azerbaijani: 'Səba' } },
+  {
+    value: 35,
+    label: { arabic: 'فاطر', russian: 'Творец', azerbaijani: 'Fatir' },
+  },
+  {
+    value: 36,
+    label: { arabic: 'يس', russian: 'Йа Син', azerbaijani: 'Yasin' },
+  },
+  {
+    value: 37,
+    label: {
+      arabic: 'الصافات',
+      russian: 'Стоящие рядами',
+      azerbaijani: 'Saffat',
+    },
+  },
+  { value: 38, label: { arabic: 'ص', russian: 'Цад', azerbaijani: 'Sad' } },
+  {
+    value: 39,
+    label: { arabic: 'الزمر', russian: 'Толпы', azerbaijani: 'Zumər' },
+  },
+  {
+    value: 40,
+    label: { arabic: 'غافر', russian: 'Прощающий', azerbaijani: 'Ğafir' },
+  },
+  {
+    value: 41,
+    label: { arabic: 'فصلت', russian: 'Разъяснённые', azerbaijani: 'Fəsilət' },
+  },
+  {
+    value: 42,
+    label: { arabic: 'الشورى', russian: 'Совет', azerbaijani: 'Şura' },
+  },
+  {
+    value: 43,
+    label: { arabic: 'الزخرف', russian: 'Украшения', azerbaijani: 'Zuxruf' },
+  },
+  {
+    value: 44,
+    label: { arabic: 'الدخان', russian: 'Дым', azerbaijani: 'Duxan' },
+  },
+  {
+    value: 45,
+    label: {
+      arabic: 'الجاثية',
+      russian: 'Наклоняющаяся',
+      azerbaijani: 'Casiyə',
+    },
+  },
+  {
+    value: 46,
+    label: {
+      arabic: 'الأحقاف',
+      russian: 'Песчаные холмы',
+      azerbaijani: 'Əhqaf',
+    },
+  },
+  {
+    value: 47,
+    label: { arabic: 'محمد', russian: 'Мухаммад', azerbaijani: 'Məhəmməd' },
+  },
+  {
+    value: 48,
+    label: { arabic: 'الفتح', russian: 'Победа', azerbaijani: 'Fətih' },
+  },
+  {
+    value: 49,
+    label: { arabic: 'الحجرات', russian: 'Комнаты', azerbaijani: 'Hucurat' },
+  },
+  { value: 50, label: { arabic: 'ق', russian: 'Каф', azerbaijani: 'Qaf' } },
+  {
+    value: 51,
+    label: {
+      arabic: 'الذاريات',
+      russian: 'Рассеиватели',
+      azerbaijani: 'Zariyat',
+    },
+  },
+  {
+    value: 52,
+    label: { arabic: 'الطور', russian: 'Гора', azerbaijani: 'Tur' },
+  },
+  {
+    value: 53,
+    label: { arabic: 'النجم', russian: 'Звезда', azerbaijani: 'Nəcəm' },
+  },
+  {
+    value: 54,
+    label: { arabic: 'القمر', russian: 'Луна', azerbaijani: 'Qəmər' },
+  },
+  {
+    value: 55,
+    label: { arabic: 'الرحمن', russian: 'Милостивый', azerbaijani: 'Rəhman' },
+  },
+  {
+    value: 56,
+    label: { arabic: 'الواقعة', russian: 'Происшествие', azerbaijani: 'Vaqiə' },
+  },
+  {
+    value: 57,
+    label: { arabic: 'الحديد', russian: 'Железо', azerbaijani: 'Hədid' },
+  },
+  {
+    value: 58,
+    label: { arabic: 'المجادلة', russian: 'Спор', azerbaijani: 'Məcaələ' },
+  },
+  {
+    value: 59,
+    label: { arabic: 'الحشر', russian: 'Сбор', azerbaijani: 'Həşr' },
+  },
+  {
+    value: 60,
+    label: {
+      arabic: 'الممتحنة',
+      russian: 'Испытуемая',
+      azerbaijani: 'Mümtəhənə',
+    },
+  },
+  { value: 61, label: { arabic: 'الصف', russian: 'Ряд', azerbaijani: 'Saff' } },
+  {
+    value: 62,
+    label: { arabic: 'الجمعة', russian: 'Пятница', azerbaijani: 'Cümə' },
+  },
+  {
+    value: 63,
+    label: {
+      arabic: 'المنافقون',
+      russian: 'Лицемеры',
+      azerbaijani: 'Münafiqun',
+    },
+  },
+  {
+    value: 64,
+    label: {
+      arabic: 'التغابن',
+      russian: 'Взаимное заблуждение',
+      azerbaijani: 'Təğabun',
+    },
+  },
+  {
+    value: 65,
+    label: { arabic: 'الطلاق', russian: 'Развод', azerbaijani: 'Talaq' },
+  },
+  {
+    value: 66,
+    label: { arabic: 'التحريم', russian: 'Запрещение', azerbaijani: 'Təhərim' },
+  },
+  {
+    value: 67,
+    label: { arabic: 'الملك', russian: 'Царство', azerbaijani: 'Mülk' },
+  },
+  {
+    value: 68,
+    label: { arabic: 'القلم', russian: 'Перо', azerbaijani: 'Qalam' },
+  },
+  {
+    value: 69,
+    label: { arabic: 'الحاقة', russian: 'Непременное', azerbaijani: 'Haaqqə' },
+  },
+  {
+    value: 70,
+    label: { arabic: 'المعارج', russian: 'Ступени', azerbaijani: 'Məa’aric' },
+  },
+  { value: 71, label: { arabic: 'نوح', russian: 'Нух', azerbaijani: 'Nuh' } },
+  {
+    value: 72,
+    label: { arabic: 'الجن', russian: 'Джинны', azerbaijani: 'Cinn' },
+  },
+  {
+    value: 73,
+    label: {
+      arabic: 'المزّمّل',
+      russian: 'Обёрнутый в покрывало',
+      azerbaijani: 'Müzzəmmil',
+    },
+  },
+  {
+    value: 74,
+    label: {
+      arabic: 'المدّثر',
+      russian: 'Закутавшийся',
+      azerbaijani: 'Müddəssir',
+    },
+  },
+  {
+    value: 75,
+    label: { arabic: 'القيامة', russian: 'Воскресение', azerbaijani: 'Qiyamə' },
+  },
+  {
+    value: 76,
+    label: { arabic: 'الإنسان', russian: 'Человек', azerbaijani: 'İnsan' },
+  },
+  {
+    value: 77,
+    label: {
+      arabic: 'المرسلات',
+      russian: 'Посланники',
+      azerbaijani: 'Mürsilat',
+    },
+  },
+  {
+    value: 78,
+    label: { arabic: 'النبأ', russian: 'Весть', azerbaijani: 'Nəbə' },
+  },
+  {
+    value: 79,
+    label: {
+      arabic: 'النازعات',
+      russian: 'Вырывающие',
+      azerbaijani: "Nazi'at",
+    },
+  },
+  {
+    value: 80,
+    label: { arabic: 'عبس', russian: 'Он нахмурился', azerbaijani: 'Abasa' },
+  },
+  {
+    value: 81,
+    label: { arabic: 'التكوير', russian: 'Скручивание', azerbaijani: 'Təkvir' },
+  },
+  {
+    value: 82,
+    label: {
+      arabic: 'الإنفطار',
+      russian: 'Раскалывание',
+      azerbaijani: 'İnfitār',
+    },
+  },
+  {
+    value: 83,
+    label: {
+      arabic: 'المطفّفين',
+      russian: 'Обвешивающие',
+      azerbaijani: 'Mutəffinun',
+    },
+  },
+  {
+    value: 84,
+    label: {
+      arabic: 'الإنشقاق',
+      russian: 'Расщепление',
+      azerbaijani: 'İnşiqaq',
+    },
+  },
+  {
+    value: 85,
+    label: { arabic: 'البروج', russian: 'Созвездия', azerbaijani: 'Büruc' },
+  },
+  {
+    value: 86,
+    label: { arabic: 'الطارق', russian: 'Ночной путник', azerbaijani: 'Tariq' },
+  },
+  {
+    value: 87,
+    label: { arabic: 'الأعلى', russian: 'Всевышний', azerbaijani: "A'la" },
+  },
+  {
+    value: 88,
+    label: {
+      arabic: 'الغاشية',
+      russian: 'Обрушивающаяся',
+      azerbaijani: 'Ğaşiya',
+    },
+  },
+  {
+    value: 89,
+    label: { arabic: 'الفجر', russian: 'Рассвет', azerbaijani: 'Fəcr' },
+  },
+  {
+    value: 90,
+    label: { arabic: 'البلد', russian: 'Город', azerbaijani: 'Bələd' },
+  },
+  {
+    value: 91,
+    label: { arabic: 'الشمس', russian: 'Солнце', azerbaijani: 'Şəms' },
+  },
+  {
+    value: 92,
+    label: { arabic: 'الليل', russian: 'Ночь', azerbaijani: 'Layl' },
+  },
+  {
+    value: 93,
+    label: { arabic: 'الضحى', russian: 'Утро', azerbaijani: 'Duhə' },
+  },
+  {
+    value: 94,
+    label: { arabic: 'الشرح', russian: 'Раскрытие', azerbaijani: 'Şərh' },
+  },
+  {
+    value: 95,
+    label: { arabic: 'التين', russian: 'Инжир', azerbaijani: 'Tiin' },
+  },
+  {
+    value: 96,
+    label: { arabic: 'العلق', russian: 'Сгусток', azerbaijani: 'Əlaq' },
+  },
+  {
+    value: 97,
+    label: { arabic: 'القدر', russian: 'Могущества', azerbaijani: 'Qədr' },
+  },
+  {
+    value: 98,
+    label: {
+      arabic: 'البينة',
+      russian: 'Ясное доказательство',
+      azerbaijani: 'Bəyinə',
+    },
+  },
+  {
+    value: 99,
+    label: {
+      arabic: 'الزلزلة',
+      russian: 'Землетрясение',
+      azerbaijani: 'Zəlzələ',
+    },
+  },
+  {
+    value: 100,
+    label: {
+      arabic: 'العاديات',
+      russian: 'Галопирующие',
+      azerbaijani: 'Adiyat',
+    },
+  },
+  {
+    value: 101,
+    label: {
+      arabic: 'القارعة',
+      russian: 'Сокрушительное событие',
+      azerbaijani: 'Qariə',
+    },
+  },
+  {
+    value: 102,
+    label: {
+      arabic: 'التكاثر',
+      russian: 'Соперничество',
+      azerbaijani: 'Təkasur',
+    },
+  },
+  {
+    value: 103,
+    label: { arabic: 'العصر', russian: 'Время', azerbaijani: 'Asr' },
+  },
+  {
+    value: 104,
+    label: { arabic: 'الهمزة', russian: 'Осуждающий', azerbaijani: 'Həməzə' },
+  },
+  {
+    value: 105,
+    label: { arabic: 'الفيل', russian: 'Слон', azerbaijani: 'Fil' },
+  },
+  {
+    value: 106,
+    label: { arabic: 'قريش', russian: 'Курайш', azerbaijani: 'Quraysh' },
+  },
+  {
+    value: 107,
+    label: {
+      arabic: 'الماعون',
+      russian: 'Малое подаяние',
+      azerbaijani: 'Məun',
+    },
+  },
+  {
+    value: 108,
+    label: { arabic: 'الكوثر', russian: 'Изобилие', azerbaijani: 'Kausar' },
+  },
+  {
+    value: 109,
+    label: {
+      arabic: 'الكافرون',
+      russian: 'Неверующие',
+      azerbaijani: 'Kafirun',
+    },
+  },
+  {
+    value: 110,
+    label: { arabic: 'النصر', russian: 'Помощь', azerbaijani: 'Nəsr' },
+  },
+  {
+    value: 111,
+    label: { arabic: 'المسد', russian: 'Верёвка', azerbaijani: 'Məsəd' },
+  },
+  {
+    value: 112,
+    label: { arabic: 'الإخلاص', russian: 'Искренность', azerbaijani: 'İxlas' },
+  },
+  {
+    value: 113,
+    label: { arabic: 'الفلق', russian: 'Рассвет', azerbaijani: 'Fələq' },
+  },
+  {
+    value: 114,
+    label: { arabic: 'الناس', russian: 'Люди', azerbaijani: 'Nəs' },
+  },
+]
--- src/features/QuranReader/ui/QuranReader/QuranReader.tsx
+++ src/features/QuranReader/ui/QuranReader/QuranReader.tsx
@@ -2,28 +2,24 @@ import { useState } from 'react'
 import styles from './QuranReader.module.scss'
 import { useGetAyahsQuery } from 'features/QuranReader/api/quranApi'
 import { AyahCardQuran } from 'entities'
-import { Spin, Select } from 'antd'
+import { Spin, Select, Form } from 'antd'
 import { Pagination } from 'features/Pagination/ui/Pagination/Pagination'
 import Favorites from 'features/Favorites/ui/Favorites/Favorites'
+import { surahOptions } from 'features/QuranReader/constants/surahs'
 
-const { Option } = Select
-
-const QuranReader: React.FC = () => {
+const QuranReader = () => {
   const [currentPage, setCurrentPage] = useState(1)
-  const [currentView, setCurrentView] = useState<'reader' | 'favorites'>(
-    'reader'
-  )
+  const [currentView, setCurrentView] = useState<'reader' | 'favorites'>('reader')
   const [chapterId, setChapterId] = useState(1)
+
   const { data, isLoading, isFetching, error } = useGetAyahsQuery(
     { chapterId, page: currentPage },
     { refetchOnMountOrArgChange: true }
   )
 
-  const handlePageChange = (newPage: number) => {
-    if (data && newPage >= 1 && newPage <= data.totalPages) {
-      setCurrentPage(newPage)
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-    }
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
   const handleChapterChange = (value: number) => {
@@ -31,55 +27,66 @@ const QuranReader: React.FC = () => {
     setCurrentPage(1)
   }
 
-  console.log('Data:', data)
-  console.log('Error:', error)
-
   return (
-    <>
-      {/* <header className={styles.header}></header> */}
-      <main className={styles.main}>
-        <Select
-          className={styles.select}
-          defaultValue={1}
-          style={{ width: 150 }}
-          onChange={handleChapterChange}>
-          {Array.from({ length: 114 }, (_, i) => (
-            <Option
-              key={i + 1}
-              value={i + 1}>
-              Сура {i + 1}
-            </Option>
-          ))}
-        </Select>
-        {currentView === 'reader' && (
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Чтение Корана</h1>
+        <div className={styles.toggle}>
+          <button
+            className={currentView === 'reader' ? styles.active : ''}
+            onClick={() => setCurrentView('reader')}>
+            Аяты
+          </button>
+          <button
+            className={currentView === 'favorites' ? styles.active : ''}
+            onClick={() => setCurrentView('favorites')}>
+            Избранное
+          </button>
+        </div>
+      </header>
+
+      <section className={styles.controls}>
+        <Form layout="vertical">
+          <Form.Item label="Сура">
+            <Select
+              className={styles.select}
+              value={chapterId}
+              onChange={handleChapterChange}
+              showSearch
+              optionFilterProp="label"
+              filterOption={(input, option) => {
+                const label = option?.label as string
+                return label.toLowerCase().includes(input.toLowerCase())
+              }}
+              options={surahOptions.map(option => ({
+                value: option.value,
+                label: `Сура ${option.value}: ${option.label.russian} (${option.label.arabic})`
+              }))}
+            />
+          </Form.Item>
+        </Form>
+      </section>
+
+      <section className={styles.content}>
+        {isLoading || isFetching ? (
+          <div className={styles.status}>
+            <Spin />
+            <p className={styles.loadingText}>Загрузка аятов...</p>
+          </div>
+        ) : error ? (
+          <div className={styles.status}>
+            <p className={styles.errorText}>
+              Ошибка: {error.message || 'Неизвестная ошибка'}
+            </p>
+          </div>
+        ) : currentView === 'reader' ? (
           <>
-            {(isLoading || isFetching) && (
-              <div className={styles.loadingContainer}>
-                <Spin className={styles.loadingSpinner} />
-                <span className={styles.loadingText}>Загрузка аятов...</span>
-              </div>
-            )}
-            {error && (
-              <div className={styles.loadingContainer}>
-                <p className={styles.errorText}>
-                  Ошибка загрузки: {error.message   || 'Неизвестная ошибка'}
-                </p>
-              </div>
-            )}
-            {data && data.ayahs?.length === 0 && (
-              <div className={styles.loadingContainer}>
-                <p className={styles.errorText}>
-                  Аяты для этой суры не найдены
-                </p>
-              </div>
-            )}
-            {data?.ayahs?.map(ayah => (
-              <AyahCardQuran
-                key={ayah.id}
-                ayah={ayah}
-              />
-            ))}
-            {data && data.totalPages > 1 && (
+            <div className={styles.ayahList}>
+              {data?.ayahs?.map(ayah => (
+                <AyahCardQuran key={ayah.id} ayah={ayah} />
+              ))}
+            </div>
+            {data?.totalPages.length > 1 && (
               <Pagination
                 currentPage={currentPage}
                 totalPages={data.totalPages}
@@ -87,10 +94,11 @@ const QuranReader: React.FC = () => {
               />
             )}
           </>
+        ) : (
+          <Favorites />
         )}
-        {currentView === 'favorites' && <Favorites />}
-      </main>
-    </>
+      </section>
+    </main>
   )
 }
 
